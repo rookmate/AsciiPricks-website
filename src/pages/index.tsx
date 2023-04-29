134e82f0fd14900941841aaee8892d36383fb998
rookmate <14072042+rookmate@users.noreply.github.com>
2023-04-29 15:52:50 +0100

    Remove old code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,48 +25,6 @@ const PRICE = 0;
 const contractAddress: string = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? "";
 
 function Home () {
-  const [isConnected, setIsConnected] = useState(false);
-  const [quantity, setQuantity] = useState<number>(1);
-
-  const { address } = useAccount();
-
-  const { config, error: contractError } = usePrepareContractWrite({
-    address: `0x${contractAddress.substring(2)}`,
-    abi: contractABI,
-    functionName: "mint",
-    args: [quantity],
-    overrides: {
-      from: address,
-      value: ethers.utils.parseEther((quantity * PRICE).toString()),
-    },
-  });
-
-  const {
-    isLoading,
-    isSuccess: isStarted,
-    error: mintError,
-    data: mintData,
-    write,
-  } = useContractWrite(config);
-
-  const { isSuccess: isMinted } = useWaitForTransaction({
-    hash: mintData?.hash,
-  });
-
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    setQuantity(newValue as number);
-  };
-
-  useEffect(() => {
-    setIsConnected(!!address);
-  }, [address]);
-
-  useEffect(() => {
-    if (isMinted) {
-      success();
-    }
-  }, [isMinted]);
-
   return (
     <>
       <MetaTags />
@@ -74,81 +32,6 @@ function Home () {
         <div className={styles.main}>
           <Logo />
           <MainContent />
-          {/*<ConnectButton showBalance={false} />
-           {isConnected && (
-            <>
-              {isMinted ? (
-                <>
-                  <div className={styles.status}>Success!</div>
-                  <div className={styles.action}>
-                    <a
-                      href={`https://opensea.io/${address}?tab=collected`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      View on OpenSea
-                    </a>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className={styles.price}>
-                    You are about to mint <strong>{quantity}</strong> ASCII PRICKS NFT{quantity > 1 && "s"}. Move the slider below to adjust the quantity.
-                  </div>
-                  <Slider
-                    style={{ color: '#e33e81' }}
-                    value={quantity}
-                    onChange={handleChange}
-                    aria-label="Quantity"
-                    valueLabelDisplay="auto"
-                    step={1}
-                    min={1}
-                    max={10}
-                    disabled={isLoading || isStarted}
-                  />
-                  <Button
-                    variant="contained"
-                    style={{ backgroundColor: '#e33e81' }}
-                    size="large"
-                    onClick={() => {
-                      write?.();
-                    }}
-                    disabled={!!contractError || isLoading || isStarted}
-                  >
-                    Mint
-                  </Button>
-                  {isLoading && (
-                    <div className={styles.status}>Waiting for approval...</div>
-                  )}
-                  {isStarted && <div className={styles.status}>Minting...</div>}
-                  {mintData && (
-                    <div className={styles.action}>
-                      <a
-                        href={`https://etherscan.io/tx/${mintData.hash}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        View transaction
-                      </a>
-                    </div>
-                  )}
-                  {contractError && (
-                    <div className={styles.error}>
-                      An error occurred while preparing the transaction. Make
-                      sure that you have enough funds and that you haven’t
-                      reached your limit of 10 tokens.
-                    </div>
-                  )}
-                  {mintError && (
-                    <div className={styles.error}>
-                      An error occurred while accessing your wallet or
-                      processing the transaction.
-                    </div>
-                  )}
-                </>
-              )}
-            </>
-          )} */}
         </div>
       </div>
     </>
