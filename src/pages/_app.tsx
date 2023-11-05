37e09cf926b3d4233b03956e996cb01527b52a6b
rookmate <14072042+rookmate@users.noreply.github.com>
2023-11-05 18:22:04 +0000

    Update appName

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ const { chains, provider, webSocketProvider } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "RainbowKit App",
+  appName: "AsciiPricks",
   chains,
 });
 
